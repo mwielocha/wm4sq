3e3f777b992088a2357c9cc95361f3c8bf8b7b01
mwielocha <mwielocha@icloud.com>
2023-10-25 01:22:30 +0200

    dep updates

--- build.sbt
+++ build.sbt
@@ -1,4 +1,3 @@
-
 // Generated with scalagen
 
 scalacOptions in ThisBuild := Seq(
@@ -16,8 +15,8 @@ scalacOptions in ThisBuild := Seq(
 lazy val root = (project in file("."))
   .enablePlugins(JavaAppPackaging)
   .settings(
-    name := "wm4sq",
-    version := "1.0",
+    name         := "wm4sq",
+    version      := "1.0",
     organization := "io.mwielocha",
     scalaVersion := "2.13.6"
   )
@@ -28,29 +27,28 @@ packageDoc / publishArtifact := false
 
 Compile / mainClass := Some("io.mwielocha.wm4sq.Main")
 
-val quillVersion             = "3.7.1"
-val biweeklyVersion          = "0.6.6"
-val akkaHttpVersion          = "10.2.3"
-val akkaVersion              = "2.6.12"
-val akkaHttpJsonVersion      = "1.36.0"
-val circeVersion             = "0.13.0"
-val catsVersion              = "2.6.0"
-val logbackVersion           = "1.2.3"
-val scalaLoggingVersion      = "3.9.3"
+val quillVersion        = "4.8.0"
+val biweeklyVersion     = "0.6.6"
+val akkaHttpVersion     = "10.2.3"
+val akkaVersion         = "2.6.12"
+val akkaHttpJsonVersion = "1.36.0"
+val circeVersion        = "0.13.0"
+val catsVersion         = "2.6.0"
+val logbackVersion      = "1.2.3"
+val scalaLoggingVersion = "3.9.3"
 
 libraryDependencies ++= Seq(
-  "io.getquill"                %% "quill-async-mysql"      % quillVersion,
-  "net.sf.biweekly"            % "biweekly"                % biweeklyVersion,
-  "ch.qos.logback"             % "logback-classic"         % logbackVersion,
-  "com.typesafe.scala-logging" %% "scala-logging"          % scalaLoggingVersion,
-  "de.heikoseeberger"          %% "akka-http-circe"        % akkaHttpJsonVersion,
-  "com.typesafe.akka"          %% "akka-http"              % akkaHttpVersion,
-  "com.typesafe.akka"          %% "akka-stream"            % akkaVersion,
-  "io.circe"                   %% "circe-core"             % circeVersion,
-  "io.circe"                   %% "circe-generic"          % circeVersion,
-  "io.circe"                   %% "circe-parser"           % circeVersion,
-  "io.circe"                   %% "circe-literal"          % circeVersion,
-  "org.typelevel"              %% "cats-core"              % catsVersion,
-  "org.scalatest"              %% "scalatest"              % "3.2.9" % "test"
+  "io.getquill"                %% "quill-jasync-mysql" % quillVersion,
+  "net.sf.biweekly"             % "biweekly"           % biweeklyVersion,
+  "ch.qos.logback"              % "logback-classic"    % logbackVersion,
+  "com.typesafe.scala-logging" %% "scala-logging"      % scalaLoggingVersion,
+  "de.heikoseeberger"          %% "akka-http-circe"    % akkaHttpJsonVersion,
+  "com.typesafe.akka"          %% "akka-http"          % akkaHttpVersion,
+  "com.typesafe.akka"          %% "akka-stream"        % akkaVersion,
+  "io.circe"                   %% "circe-core"         % circeVersion,
+  "io.circe"                   %% "circe-generic"      % circeVersion,
+  "io.circe"                   %% "circe-parser"       % circeVersion,
+  "io.circe"                   %% "circe-literal"      % circeVersion,
+  "org.typelevel"              %% "cats-core"          % catsVersion,
+  "org.scalatest"              %% "scalatest"          % "3.2.9" % "test"
 )
-
--- src/main/scala/io/mwielocha/wm4sq/routes/Routes.scala
+++ src/main/scala/io/mwielocha/wm4sq/routes/Routes.scala
@@ -1,9 +1,12 @@
 package io.mwielocha.wm4sq.routes
 
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpRequest, _}
+import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.Location
-import akka.http.scaladsl.server.Directives.{get, path, _}
+import akka.http.scaladsl.server.Directives.get
+import akka.http.scaladsl.server.Directives.path
+import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import cats.data.OptionT
@@ -12,20 +15,25 @@ import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
 import de.heikoseeberger.akkahttpcirce.ErrorAccumulatingCirceSupport
 import io.circe.Json
-import io.getquill.{MysqlAsyncContext, SnakeCase}
+import io.getquill.context.jasync.Decoders
+import io.getquill.mirrorContextWithQueryProbing.Decoder
+import io.getquill.MappedEncoding
+import io.getquill.MysqlJAsyncContext
+import io.getquill.SnakeCase
 import io.mwielocha.wm4sq.ical.ICalendarSupport
 import io.mwielocha.wm4sq.ical.Implicits._
 import io.mwielocha.wm4sq.model.forsquare.Checkins
-import io.mwielocha.wm4sq.model.{AccessToken, User}
+import io.mwielocha.wm4sq.model.AccessToken
+import io.mwielocha.wm4sq.model.User
 
 import scala.concurrent.Future
 
 class Routes(config: Config) extends ErrorAccumulatingCirceSupport with ICalendarSupport with LazyLogging {
 
-  private val ctx = new MysqlAsyncContext(SnakeCase, "db")
+  private val ctx = new MysqlJAsyncContext(SnakeCase, "db")
 
-  private val host = config.getString("host")
-  private val clientId = config.getString("4sq.client_id")
+  private val host         = config.getString("host")
+  private val clientId     = config.getString("4sq.client_id")
   private val clientSecret = config.getString("4sq.client_secret")
 
   import ctx._
@@ -39,18 +47,18 @@ class Routes(config: Config) extends ErrorAccumulatingCirceSupport with ICalenda
               complete {
 
                 for {
-                  user <- Future.successful(User())
-                  q = quote {
-                    query[User]
-                      .insert(lift(user))
-                  }
-                  _ <- ctx.run(q)
+                  user  <- Future.successful(User())
+                  q      = quote {
+                             query[User]
+                               .insertValue(lift(user))
+                           }
+                  _     <- ctx.run(q)
                   params = Uri.Query(
-                    "client_id" -> clientId,
-                    "response_type" -> "code",
-                    "redirect_uri" -> s"$host/finalize?id=${user.id}"
-                  )
-                  uri = Uri("https://foursquare.com/oauth2/authenticate").withQuery(params)
+                             "client_id"     -> clientId,
+                             "response_type" -> "code",
+                             "redirect_uri"  -> s"$host/finalize?id=${user.id}"
+                           )
+                  uri    = Uri("https://foursquare.com/oauth2/authenticate").withQuery(params)
                 } yield HttpResponse(status = StatusCodes.TemporaryRedirect, headers = Location(uri) :: Nil)
               }
             }
@@ -58,28 +66,28 @@ class Routes(config: Config) extends ErrorAccumulatingCirceSupport with ICalenda
             complete {
 
               val params = Uri.Query(
-                "client_id" -> clientId,
+                "client_id"     -> clientId,
                 "client_secret" -> clientSecret,
-                "grant_type" -> "authorization_code",
-                "redirect_uri" -> s"$host/finalize?id=$id",
-                "code" -> code
+                "grant_type"    -> "authorization_code",
+                "redirect_uri"  -> s"$host/finalize?id=$id",
+                "code"          -> code
               )
 
               for {
-                response <- Http().singleRequest(
-                  HttpRequest(
-                    method = HttpMethods.GET,
-                    uri = Uri("https://foursquare.com/oauth2/access_token").withQuery(params)
-                  )
-                )
-                _ = logger.info(s"Oauth response: " + response)
+                response    <- Http().singleRequest(
+                                 HttpRequest(
+                                   method = HttpMethods.GET,
+                                   uri = Uri("https://foursquare.com/oauth2/access_token").withQuery(params)
+                                 )
+                               )
+                _            = logger.info(s"Oauth response: " + response)
                 accessToken <- Unmarshal(response.entity).to[AccessToken]
-                q = quote {
-                  query[User]
-                    .filter(_.id == lift(id))
-                    .update(_.token -> lift(Option(accessToken.access_token)))
-                }
-                _ <- ctx.run(q)
+                q            = quote {
+                                 query[User]
+                                   .filter(_.id == lift(id))
+                                   .update(_.token -> lift(Option(accessToken.access_token)))
+                               }
+                _           <- ctx.run(q)
               } yield Json.fromString(id)
             }
 
@@ -93,23 +101,23 @@ class Routes(config: Config) extends ErrorAccumulatingCirceSupport with ICalenda
                 }
 
                 (for {
-                  users <- OptionT.liftF(ctx.run(q))
-                  user <- OptionT.fromOption[Future](users.headOption)
-                  token <- OptionT.fromOption[Future](user.token)
-                  params = Uri.Query(
-                    "v" -> "20190423",
-                    "oauth_token" -> token,
-                    "limit" -> "100000"
-                  )
-                  request = HttpRequest(
-                    method = HttpMethods.GET,
-                    uri = Uri("https://api.foursquare.com/v2/users/self/checkins").withQuery(params)
-                  )
+                  users    <- OptionT.liftF(ctx.run(q))
+                  user     <- OptionT.fromOption[Future](users.headOption)
+                  token    <- OptionT.fromOption[Future](user.token)
+                  params    = Uri.Query(
+                                "v"           -> "20190423",
+                                "oauth_token" -> token,
+                                "limit"       -> "100000"
+                              )
+                  request   = HttpRequest(
+                                method = HttpMethods.GET,
+                                uri = Uri("https://api.foursquare.com/v2/users/self/checkins").withQuery(params)
+                              )
                   response <- OptionT.liftF(Http().singleRequest(request))
                   checkins <- OptionT.liftF {
-                    Unmarshal(response.entity).to[Checkins]
-                  }
-                  _ = logger.info(s"Got ${checkins.count} checkins...")
+                                Unmarshal(response.entity).to[Checkins]
+                              }
+                  _         = logger.info(s"Got ${checkins.count} checkins...")
                 } yield checkins.asCalendar).value
               }
             }
